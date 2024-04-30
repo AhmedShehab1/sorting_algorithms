32c58e7c95375041fc491e6f0602ac696d85e224
Mazen Atlam <136184080+MazenAtlam@users.noreply.github.com>
2024-05-01 00:01:11 +0300

    Update 3-quick_sort.c

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -1,66 +1,83 @@
 #include "sort.h"
 
-void quick_sort_2(int *array, size_t size, int low, int high)
+/**
+ * partition - A function that seperate an array to 2 unsorted parts:
+ *              The first has all elements less than a pivot and the second
+ *              has all elements greater than or equal to this pivot
+ *
+ * @array: A pointer to the array to be sorted
+ * @size: The size of the array
+ * @start: The beginnig index of the part to be sorted
+ * @end: The end index of the part to be sorted
+ *
+ * Return: The index of the pivot
+ */
+size_t partition(int *array, size_t size, int start, int end)
 {
-	int pivotNewIndex;
+	size_t index, curr_index = start;
+	int temp, pivot = array[end];
 
-	if (high > low)
+	for (index = start; (int) index < end; index++)
 	{
-		pivotNewIndex = partition(array, low, high, size);
-		quick_sort_2(array, size, low, pivotNewIndex - 1);
-		quick_sort_2(array, size, pivotNewIndex + 1, high);
+		if (array[index] < pivot)
+		{
+			if (curr_index != index)
+			{
+				temp = array[index];
+				array[index] = array[curr_index];
+				array[curr_index] = temp;
+				print_array((const int *) array, size);
+			}
+			curr_index++;
+		}
+	}
+	if (array[curr_index] != pivot)
+	{
+		array[end] = array[curr_index];
+		array[curr_index] = pivot;
+		print_array((const int *) array, size);
+	}
+
+	return (curr_index);
+}
+
+/**
+ * lomuto_scheme - A funtion that sorts an array of integers
+ *              in ascending order using the Quick sort algorithm
+ *              and implement the Lomuto partition scheme
+ *
+ * @array: A pointer to the array to be sorted
+ * @size: The size of the array
+ * @start: The beginnig index of the part to be sorted
+ * @end: The end index of the part to be sorted
+ *
+ * Return: Nothing
+ */
+void lomuto_scheme(int *array, size_t size, int start, int end)
+{
+	int pivot;
+
+	if (end > start)
+	{
+		pivot = partition(array, size, start, end);
+		lomuto_scheme(array, size, start, (pivot - 1));
+		lomuto_scheme(array, size, (pivot + 1), end);
 	}
 }
 
 /**
- * quick_sort - Sorts Array Using Quick Sort
- * Algorithm (Lomuto partition scheme)
- * @array: Array To Be Sorted
- * @size: Size Of The Array
+ * quick_sort - A function that sorts an array of integers
+ *		in ascending order using the Quick sort algorithm
  *
-*/
+ * @array: A pointer to the array to be sorted
+ * @size: The size of the array
+ *
+ * Return: Nothing
+ */
 void quick_sort(int *array, size_t size)
 {
 	if (!array || size < 2)
 		return;
 
-	quick_sort_2(array, size, 0, size - 1);
-}
-
-/**
- * partition - Lomuto Partition Scheme
- * @array: Array To Be Sorted
- * @low: Lower Bound Of The Array
- * @high: Upper Bound Of The Array
- * @size: Size Of The Array
- *
- * Return: New Position Of Pivot Element
-*/
-int partition(int *array, int low, int high, int size)
-{
-	size_t i, index = low;
-	int pivot = array[high], temp;
-
-	for (i = low; (int) i < high; i++)
-	{
-		if (array[i] < pivot)
-		{
-			if (index != i)
-			{
-				temp = array[i];
-				array[i] = array[index];
-				array[index] = temp;
-				print_array(array, size);
-			}
-			index++;
-		}
-	}
-	if (array[index] != array[high])
-	{
-		array[index] = array[high];
-		array[high] = pivot;
-		print_array(array, size);
-	}
-
-	return (index);
+	lomuto_scheme(array, size, 0, (size - 1));
 }
